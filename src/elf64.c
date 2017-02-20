8611d259e8766717e347f37f0ac08f8608def5ac
budkahaw <budkahaw@indiana.edu>
2017-02-20 14:42:19 -0500

    Removing logs

--- src/elf64.c
+++ src/elf64.c
@@ -32,35 +32,35 @@ ELF *elf64_read(char *e_fname) {
 	bin->size = ftell(bin->file);
 
 	/* Read elf header */	
-	std_out("[*] Read ELF header\n");
+	// std_out("[*] Read ELF header\n");
 	bin->ehr = read_header(bin->file);
 	/* Read section header table */
-	std_out("[*] Read section header table\n");
+	// std_out("[*] Read section header table\n");
 	bin->shr = read_shr_all(bin->file, bin->ehr->e_shnum, 
 			bin->ehr->e_shoff, bin->ehr->e_shentsize);
 	/* Read program header table */
-	std_out("[*] Read program header table\n");
+	// std_out("[*] Read program header table\n");
 	bin->phr = Elf64_read_phr_all(bin->file, bin->ehr->e_phnum, 
 			bin->ehr->e_phoff, bin->ehr->e_phentsize);
 	/* Get symbol table index */
-	std_out("[*] Look for symbol table\n");
+	// std_out("[*] Look for symbol table\n");
 	bin->symtab_idx = get_section_idx(bin, ".symtab");
 	if (bin->symtab_idx == 0xFFFF)
 		std_out("[*] /!\\ No symbol table found\n");
 	else {
 		/* Read symbol table */
-		std_out("[*] Read symbol table\n");
+		// std_out("[*] Read symbol table\n");
 		bin->symtab = read_sym_tab(bin->file, bin->shr[bin->symtab_idx], 
 							&(bin->symtab_num));
 	}
 	/* Get dynamic symbol table index */
-	std_out("[*] Look for dynamic symbol table\n");
+	// std_out("[*] Look for dynamic symbol table\n");
 	bin->dynsym_idx = get_section_idx(bin, ".dynsym");
 	if (bin->symtab_idx == 0xFFFF)
 		std_out("[*] /!\\ No dynamic symbol table found\n");
 	else {
 		/* Read dynamic symbol table */
-		std_out("[*] Read dynamic symbol table\n");
+		// std_out("[*] Read dynamic symbol table\n");
 		bin->dynsym = read_sym_tab(bin->file, bin->shr[bin->dynsym_idx], 
 							&(bin->dynsym_num));
 	}
